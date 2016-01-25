d200502af42d5565dbcad70505ba2552a371fa27
tom <harre096@umn.edu>
2016-01-24 23:57:00 -0600

    retyped and commented the blink code

--- Assignment1/blink.c
+++ Assignment1/blink.c
@@ -25,27 +25,32 @@ void main(void)
 
 }*/
 
-/*  Kevin Example
-void main ( void)
-{
-	WDTCTL = WDTPW | WDTHOLD; // S top wa tchdog t ime r
-	///∗ I n i t i a l i z e t h e I /O p o r t ∗/
-	P1DIR|=BIT0 ;
-	if (P1SEL0 & BIT0 )
-	{
-		if (P1SEL1 & BIT0 )
-		P1SELC|=BIT0 ;
-		else
-		P1SEL0&=~BIT0 ;
+/*  Kevin Example */
+void main(void){
+	WDTCTL = WDTPW | WDTHOLD; //Stop watchdog timer
+	// Initiaze the I/O port
+	P1DIR|=BIT0;
+
+	//Set Port 1's Selectors to both be zero
+	if (P1SEL0 & BIT0){ //If bit zero is 1 SEL0
+		if(P1SEL1 & BIT0){
+			P1SELC|=BIT0;  //Both are 1, change both to zero using complement.
+			// This is janky imo. See 10.2.6 in user guide :)
+		}else{
+			P1SEL0&=~BIT0;
+		}
+	} else if (P1SEL1 & BIT0){ //If bit zero is 1 SEL1
+		P1SEL1&=~BIT0;
 	}
-	else if (P1SEL1 & BIT0 )
-	P1SEL1&=~BIT0 ;
-	P1OUT|=BIT0 ; // t u rn t h e LED on
-	///∗ Now we e n t e r t h e l o o p ∗/
-	for ( ; ; ) // i d i om a t i c i n f i n i t e l o o p ; w h i l e ( 1 ) i s a l s o u s a b l e he re
-	{
-		volatile int k=0;
-		for ( k=0;k<100000;++k ) ;
-		P1OUT^=BIT0 ;
+
+	//Turn LED on
+	P1OUT|=BIT0;
+
+	//Enter loop
+	while(1){
+		volatile int k=0; //Using volatile to trick complier into letting empty loop run
+		for (k = 0; k < 20000; ++k);
+		P1OUT^=BIT0; //Use Xor to flip bit0
 	}
-}*/
+
+}
