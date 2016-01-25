a92ce379b1f8f3c42b80e8fe4c4478f1f871ea48
tom <harre096@umn.edu>
2016-01-24 23:35:59 -0600

    adding both online example's code and paste of Kevin's code

--- Assignment1/blink.c
+++ Assignment1/blink.c
@@ -5,7 +5,8 @@
 //****************************************************************************
 
 #include "msp432.h"
-
+/*
+ *Online Example:
 void main(void)
 {
     volatile uint32_t i;
@@ -20,4 +21,31 @@ void main(void)
         P1OUT ^= BIT0;                  // Toggle P1.0
         for(i=30000; i>0; i--);         // Delay
     }
-}
+    P1DIR |= BIT0;
+
+}*/
+
+/*  Kevin Example
+void main ( void)
+{
+	WDTCTL = WDTPW | WDTHOLD; // S top wa tchdog t ime r
+	///∗ I n i t i a l i z e t h e I /O p o r t ∗/
+	P1DIR|=BIT0 ;
+	if (P1SEL0 & BIT0 )
+	{
+		if (P1SEL1 & BIT0 )
+		P1SELC|=BIT0 ;
+		else
+		P1SEL0&=~BIT0 ;
+	}
+	else if (P1SEL1 & BIT0 )
+	P1SEL1&=~BIT0 ;
+	P1OUT|=BIT0 ; // t u rn t h e LED on
+	///∗ Now we e n t e r t h e l o o p ∗/
+	for ( ; ; ) // i d i om a t i c i n f i n i t e l o o p ; w h i l e ( 1 ) i s a l s o u s a b l e he re
+	{
+		volatile int k=0;
+		for ( k=0;k<100000;++k ) ;
+		P1OUT^=BIT0 ;
+	}
+}*/
