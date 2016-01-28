ae1bbb28eead9d731a2aa28b6e75669389efb689
tom <harre096@umn.edu>
2016-01-27 22:37:09 -0600

    some renaming

--- Assignment1/Assignment1.c
+++ Assignment1/Assignment1.c
@@ -1,7 +1,7 @@
 #include "msp432.h"
 
-void setSelectToZeroP1(char bitToSet){
-	//Set Port1, Line[bitToSet]'s Selectors to both be zero (Digital I/O mode)
+void selectDIO_P1(char bitToSet){
+	//Set Port1, Line[bitToSet]'s selectors to both be zero (Digital I/O mode)
 	if (P1SEL0 & bitToSet){
 		if(P1SEL1 & bitToSet){
 			P1SELC|=bitToSet;  //Both 1 => both to 0 using complement. 10.2.6 in user guide
@@ -12,8 +12,8 @@ void setSelectToZeroP1(char bitToSet){
 		P1SEL1&=~bitToSet;
 	}
 }
-void setSelectToZeroP2(char bitToSet){
-	//Set Port2, Line[bitToSet]'s Selectors to both be zero (Digital I/O mode)
+void selectDIO_P2(char bitToSet){
+	//Set Port2, Line[bitToSet]'s selectors to both be zero (Digital I/O mode)
 	if (P2SEL0 & bitToSet){
 		if(P2SEL1 & bitToSet){
 			P2SELC|=bitToSet;  //Both 1 => both to 0 using complement. 10.2.6 in user guide
@@ -28,20 +28,20 @@ void setSelectToZeroP2(char bitToSet){
 
 void initLED(void){
 	P2DIR|=BIT0|BIT1|BIT2;  //1 aka "out" for LED2 on lines 0,1,2
-	setSelectToZeroP2(BIT0);
-	setSelectToZeroP2(BIT1);
-	setSelectToZeroP2(BIT2);
+	selectDIO_P2(BIT0);
+	selectDIO_P2(BIT1);
+	selectDIO_P2(BIT2);
 }
 
 
 void initButtons(void){
-	P1DIR&=~(BIT1&BIT4); //0 aka "in" for button on line 1 and 4
+	P1DIR&=~(BIT1&BIT4); //0 aka "in" for button on lines 1,4
 
-	P1REN|=BIT1|BIT4;  //enaling internal pull-up/pull-down resistor
-	P1OUT|=BIT1|BIT4;  //default circuit to pull-up
+	P1REN|=BIT1|BIT4;  //enaling internal pull-up/pull-down resistors
+	P1OUT|=BIT1|BIT4;  //default circuit to pull-ups
 
-	setSelectToZeroP1(BIT1);
-	setSelectToZeroP1(BIT4);
+	selectDIO_P1(BIT1);
+	selectDIO_P1(BIT4);
 }
 
 void newColor (unsigned int *colorState){
@@ -61,16 +61,16 @@ void main(void){
 
 
 	//Enter loop
-	unsigned int autoModeState = 1;
+	unsigned int autoState = 1;
 	unsigned int colorState = 0;
 
 	while(1){
 		unsigned char portIn = P1IN;
 		if(!(portIn & BIT1)){ //if portIn BIT1 is 0, toggle automatic color changing
-			autoModeState^=1;
+			autoState^=1;
 		}
 
-		if(autoModeState){
+		if(autoState){
 			setColor(colorState);
 			newColor(&colorState);
 		} else if (!(portIn & BIT4)){ //if portIn BIT4 is 0, change color manually
