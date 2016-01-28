366f92e1fb00eb8587382ea69b101f4f570aa420
tom <harre096@umn.edu>
2016-01-28 15:55:14 -0600

    working through changes.

--- Assignment1/Assignment1.c
+++ Assignment1/Assignment1.c
@@ -6,24 +6,51 @@ void PortOneInterrupt(void) {
 	P1OUT^=BIT0;
 }
 
-void selectDIO_P1(char bitToSet){
-	//Set Port1, Line[bitToSet]'s selectors to both be zero (Digital I/O mode)
-	if (P1SEL0 & bitToSet){
-		if(P1SEL1 & bitToSet){
-			P1SELC|=bitToSet;  //Both 1 => both to 0 using complement. 10.2.6 in user guide
+void selectPortFunction(int port, int line, int sel10, int sel1){
+	//(p,l,0,0) will set port to Digital I/O
+	if(port==1){
+		if(P1SEL0 & BIT(line)!=sel10){
+			if(P1SEL1 & BIT(line)!=sel1){
+				P1SELC|=BIT(line);
+			}else{
+				P1SEL0^=BIT(line);
+			}
 		}else{
-			P1SEL0&=~bitToSet;
+			if(P1SEL1 & BIT(line)!=sel1)
+				P1SEL1^=BIT(line);
 		}
-	} else if (P2SEL1 & bitToSet){
-		P1SEL1&=~bitToSet;
-	}
-}
+	}else{
+		if(P2SEL0 & BIT(line)!=sel10){
+			if(P2SEL1 & BIT(line)!=sel1){
+				P2SELC|=BIT(line);
+			}else{
+				P2SEL0^=BIT(line);
+			}
+		}else{
+			if(P2SEL1 & BIT(line)!=sel1)
+				P2SEL1^=BIT(line);
+		}
+	})
 
 void initLED(void){
 	P1DIR|=BIT0;  //1 aka "out" for LED1 on lines 0
-	selectDIO_P1(BIT0);
+	selectPortFunction(1,0,0,0);
+	P1OUT|=BIT0;
 }
 
+void setClockFrequency(void){
+	CSKEY=0x695A;
+	CSCTL1=0x00000223;
+	CSCLKEN=0x0000800F;
+	CSKEY=0xA596;
+}
+
+void configureTimer(void){
+	TA0CTL=0x0100;
+	TA0CCTL0=0x2000
+	TA0CCR0=0xFA00; //or TA0CCRO=64000
+	TA0TL=0x0116;
+}
 
 void initButtons(void){
 	P1DIR&=~(BIT1&BIT4); //0 aka "in" for button on lines 1,4
